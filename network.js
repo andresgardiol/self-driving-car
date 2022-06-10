e4b754606ccabd71bddef6e92fd1684fe01df109
Andres Gardiol <andresgardiol@gmail.com>
2022-06-10 02:50:41 -0300

    Only draw best car sensor

--- network.js
+++ network.js
@@ -25,16 +25,15 @@ class NeuralNetwork {
                     level.biases[i],
                     Math.random() * 2 - 1,
                     amount
-                );
+                )
             }
-
             for (let i = 0; i < level.weights.length; i++) {
                 for (let j = 0; j < level.weights[i].length; j++) {
                     level.weights[i][j] = lerp(
                         level.weights[i][j],
                         Math.random() * 2 - 1,
                         amount
-                    );
+                    )
                 }
             }
         });
