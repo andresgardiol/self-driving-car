eaf09a6a0f43439a6f2fa24e6b1f31f2bedf3dd8
Andres Gardiol <andresgardiol@gmail.com>
2022-06-12 23:22:34 -0300

    Red dummy cars

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ const networkCtx = networkCanvas.getContext("2d");
 const LANE_COUNT = 4;
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9, LANE_COUNT);
 
-const N = 1000;
-const MUTATION_RATE = 0.33;
+const N = 600;
+const MUTATION_RATE = 0.14;
 const CAR_MAX_SPEED = 7;
 const cars = generateCars(N);
 let bestCar = cars[0];
@@ -87,10 +87,10 @@ function generateTraffic(N) {
     let carsQty = 0;
     for (let i = 0; i < N; i++) {
         for (let j = 0; j < LANE_COUNT; j++) {
-            let spawnCar = getRandomBetween(0, 1) ? true : false;
+            let spawnCar = !!getRandomBetween(0, 1);
             console.log(spawnCar);
-            if (spawnCar && carsQty < LANE_COUNT - 1) {
-                traffic.push(new Car(road.getLaneCenter(j), distanceBetweenEeach, 30, 50, "DUMMY", 2));
+            if (spawnCar && carsQty < LANE_COUNT - 2) {
+                traffic.push(new Car(road.getLaneCenter(j), distanceBetweenEeach, 30, 50, "DUMMY", 2, "red"));
                 carsQty++;
             }
         }
