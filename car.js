1065a6bce171071f7987be1c862671a3e39cfa6b
Andres Gardiol <andresgardiol@gmail.com>
2022-06-12 21:31:49 -0300

    Better car

--- car.js
+++ car.js
@@ -2,7 +2,7 @@ const TURN_ANGLE = 0.04;
 const FRICTION = 0.05;
 const ACCELERATION = 0.2;
 class Car {
-	constructor(x, y, width, height, controlType, maxSpeed = 5) {
+	constructor(x, y, width, height, controlType, maxSpeed = 5, color="blue") {
 		this.x = x;
 		this.y = y;
 		this.width = width;
@@ -24,6 +24,9 @@ class Car {
 			);
 		}
 		this.controls = new Controls(controlType);
+
+		this.img = new Image();
+		this.img.src="car.png";
 	}
 
 	update(roadBorders, traffic) {
@@ -124,18 +127,16 @@ class Car {
 		this.y -= Math.cos(this.angle) * this.speed;
 	}
 
-	draw(ctx, color, drawSensor = false) {
-		if (this.damaged) {
-			ctx.fillStyle = "gray";
-		} else {
-			ctx.fillStyle = color;
-		}
-		ctx.beginPath();
-		ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
-		for (let i = 1; i < this.polygon.length; i++) {
-			ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
-		}
-		ctx.fill();
+	draw(ctx, drawSensor = false) {
+		ctx.save();
+		ctx.translate(this.x, this.y);
+		ctx.rotate(-this.angle);
+		ctx.drawImage(this.img,
+				-this.width/2,
+				-this.height/2,
+				this.width,
+				this.height);
+		ctx.restore();
 
 		if (this.sensor && drawSensor) {
 			this.sensor.draw(ctx);
